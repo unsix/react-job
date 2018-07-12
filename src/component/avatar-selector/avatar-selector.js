1b00cd7c21c5a1b130260652a3ed7dd5f90113d6
unsix <UnWelt@qq.com>
2018-07-12 16:18:36 +0800

    更新

--- src/component/avatar-selector/avatar-selector.js
+++ src/component/avatar-selector/avatar-selector.js
@@ -1,13 +1,19 @@
 import React from 'react'
-
+import { Grid } from 'antd-mobile';
 
 class AvatarSelector extends React.Component{
 
   render() {
-    	const avatarList = 'boy,girl,man,woman,bull,chick,crab,hedgehog,hippopotamus,koala,lemur,pig,tiger,whale,zebra'
+    const avatarList = 'boy,girl,man,woman,bull,chick,crab,hedgehog,hippopotamus,koala,lemur,pig,tiger,whale,zebra'
+                        .split(",")
+                        .map(v=>({
+                          icon:require(`../img/${v}.png`),
+                          text:v
+                        }))
     return (
       <div>
-        <h1>选择头像页</h1>
+        <Grid data={avatarList} columnNum={5} />
+        头像选择
       </div>)
   }
 }
