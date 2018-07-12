1b1cb0cbceab8017ac7b8771c9a2b1096bae5d38
unsix <UnWelt@qq.com>
2018-07-12 16:05:43 +0800

    更新

---
+++ src/component/avatar-selector/avatar-selector.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+class AvatarSelector extends React.Component{
+
+  render() {
+    	const avatarList = 'boy,girl,man,woman,bull,chick,crab,hedgehog,hippopotamus,koala,lemur,pig,tiger,whale,zebra'
+    return (
+      <div>
+        <h1>选择头像页</h1>
+      </div>)
+  }
+}
+
+
+export default AvatarSelector
--- src/container/bossinfo/bossinfo.js
+++ src/container/bossinfo/bossinfo.js
@@ -1,8 +1,18 @@
 import React from 'react'
-import { NavBar, Icon } from 'antd-mobile';
-
+import { NavBar, Icon ,InputItem,TextareaItem} from 'antd-mobile';
+import AvatarSelector from '../../component/avatar-selector/avatar-selector'
 class BossInfo extends React.Component{
-
+  constructor(props) {
+    super(props)
+    this.state = {
+      title:''
+    }
+  }
+  onChange(key,val) {
+    this.setState({
+      [key] : val
+    })
+  }
   render() {
     return (
       <div>
@@ -13,8 +23,26 @@ class BossInfo extends React.Component{
             // <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
             // <Icon key="1" type="ellipsis" />,
           ]}
-      >NavBar
-      </NavBar>
+      >BOSS完善信息页</NavBar>
+      <AvatarSelector></AvatarSelector>
+      <InputItem onChange={(v)=>this.onChange('title',v)}>
+        招聘职位
+      </InputItem>
+      <InputItem onChange={(v)=>this.onChange('company',v)}>
+        公司名称
+      </InputItem>
+      <InputItem onChange={(v)=>this.onChange('money',v)}>
+        职位薪资
+      </InputItem>
+      <TextareaItem
+        onChange={(v)=>this.onChange('desc',v)}
+        rows={3}
+        autoHeight
+        title='职位要求'
+      >
+      
+      </TextareaItem>
+
       </div>)
   }
 }
