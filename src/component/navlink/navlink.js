8ec36feddfd77227f5fdafb45ff86523d5b5dbc6
unsix <UnWelt@qq.com>
2018-07-19 14:27:52 +0800

    更新

--- src/component/navlink/navlink.js
+++ src/component/navlink/navlink.js
@@ -1,12 +1,22 @@
 import React from 'react'
-
+import {TabBar} from 'antd-mobile'
 class NavLinkBar extends React.Component{
 
   render(){
     const navList = this.props.data.filter(v=>!v.hide)
     console.log(navList)
     return(
-      <div>dasd</div>
+      <TabBar>
+        {navList.map(v=(
+          <TabBar.Item
+            key={v.path}
+            title={v.text}
+            icon={}
+          >
+
+          </TabBar.Item>
+        ))}
+      </TabBar>
     )
   }
 }
