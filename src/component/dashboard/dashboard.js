97f3af3a3a6a6f04668560f0248662fc64e41900
unsix <UnWelt@qq.com>
2018-07-20 08:22:10 +0800

    更新

--- src/component/dashboard/dashboard.js
+++ src/component/dashboard/dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {NavBar} from 'antd-mobile'
+import {Switch, Route} from 'react-router-dom'
 import NavLinkBar from '../navlink/navlink'
 function Boss(){
   return <h2>boss首页</h2>
@@ -19,51 +20,63 @@ function User(){
 @connect(
   state=>state
 )
-class DashBoard extends React.Component{
+class Dashboard extends React.Component{
 
-  render(){
-    console.log(this.props)
-    const {pathname} = this.props.location
-    const user = this.props.user
-    const navList = [
-      {
-        path:'/boss',
-        text:'牛人',
-        icon:'boss',
-        title:'牛人列表',
-        component:Boss,
-        hide:user.type=='genius'
-      },
-      {
-        path:'/genius',
-        text:'boss',
-        icon:'job',
-        title:'Boss列表',
-        component:Genius,
-        hide:user.type=='boss'
-      },
-      {
-        path:'/msg',
-  			text:'消息',
-  			icon:'msg',
-  			title:'消息列表',
-  			component:Msg
-      },
-      {
-        path:'/me',
-        text:'我',
-        icon:'user',
-        title:'个人中心',
-        component:User
-    }
-    ]
+	render(){
+		const {pathname} = this.props.location
+		const user = this.props.user
+		const navList = [
+			{
+				path:'/boss',
+				text:'牛人',
+				icon:'boss',
+				title:'牛人列表',
+				component:Boss,
+				hide:user.type=='genius'
+			},
+			{
+				path:'/genius',
+				text:'boss',
+				icon:'job',
+				title:'BOSS列表',
+				component:Genius,
+				hide:user.type=='boss'
+			},
+			{
+				path:'/msg',
+				text:'消息',
+				icon:'msg',
+				title:'消息列表',
+				component:Msg
+			},
+			{
+				path:'/me',
+				text:'我',
+				icon:'user',
+				title:'个人中心',
+				component:User
+			}
+		]
 
-    return(
-      <div>
-        <NavBar mode='dard'>{navList.find(v=>v.path==pathname).title}</NavBar>
-        <NavLinkBar data={navList}></NavLinkBar>
-      </div>
-    )
-  }
+
+		return (
+			<div>
+				<NavBar mode='dard'>{navList.find(v=>v.path==pathname).title}</NavBar>
+        <div style={{marginTop:45}}>
+          <Switch>
+            {navList.map(v=>(
+              <Route key={v.path} path={v.path} component={v.component}></Route>
+            ))}
+          </Switch>
+        </div>
+				<NavLinkBar data={navList}></NavLinkBar>
+
+			</div>
+		)
+
+
+	}
+
 }
-export default DashBoard
+
+export default Dashboard
--- src/component/navlink/navlink.js
+++ src/component/navlink/navlink.js
@@ -1,24 +1,32 @@
 import React from 'react'
 import {TabBar} from 'antd-mobile'
+import {withRouter} from 'react-router-dom'
+
+@withRouter
 class NavLinkBar extends React.Component{
+	render(){
 
-  render(){
-    const navList = this.props.data.filter(v=>!v.hide)
-    console.log(navList)
-    return(
-      <TabBar>
-        {navList.map(v=(
-          <TabBar.Item
-            key={v.path}
-            title={v.text}
-            icon={}
-          >
+		const navList = this.props.data.filter(v=>!v.hide)
+		const {pathname} = this.props.location
+		return (
+			<TabBar>
+				{navList.map(v=>(
+					<TabBar.Item
+						key={v.path}
+						title={v.text}
+						icon={{uri: require(`./img/${v.icon}.png`)}}
+						selectedIcon={{uri: require(`./img/${v.icon}-active.png`)}}
+						selected={pathname===v.path}
+						onPress={()=>{
+							this.props.history.push(v.path)
+						}}
+					>
 
-          </TabBar.Item>
-        ))}
-      </TabBar>
-    )
-  }
+					</TabBar.Item>
+				))}
+			</TabBar>
+		)
+	}
 }
 
 export default NavLinkBar
