91dc7d9fef959346c7f172122da84bbeb8bbf841
unsix <UnWelt@qq.com>
2018-07-30 08:54:37 +0800

    更新

--- src/component/boss/boss.js
+++ src/component/boss/boss.js
@@ -1,23 +1,17 @@
 import React from 'react'
 import axios from 'axios'
+import {connect} from 'react-redux'
 import { Card, WingBlank, WhiteSpace } from 'antd-mobile'
+import {getUserList} from '../../redux/chatuser_redux'
+
+@connect (
+  state=>state.chatuser,
+  {getUserList}
+)
 
 class Boss extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      data:[]
-    }
-  }
   componentDidMount(){
-    axios.get('/user/list?type=genius')
-         .then(res=>{
-           if(res.data.code==0){
-             this.setState({data:res.data.data})
-           }
-         })
-
-
+    this.props.getUserList('genius')
   }
   render(){
     const Header = Card.Header
@@ -27,7 +21,7 @@ class Boss extends React.Component{
       <WingBlank>
 			<WhiteSpace></WhiteSpace>
 		    {
-          this.state.data.map(v=>(
+          this.props.userlist.map(v=>(
             v.avatar?(
             <Card key={v._id}>
               <Header
