9f86314c20ed00b0d2db1fe461e474c4fb44c201
unsix <UnWelt@qq.com>
2018-09-15 21:25:51 +0800

    消息列表

--- src/component/msg/msg.js
+++ src/component/msg/msg.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
+@connect(
+    state=>state
+)
 class Msg extends React.Component{
-
+    
     render(){
+        console.log(this.props)
+        const msgGroup = {}
+        this.props.chat.chatmsg.forEach(v=>{
+            msgGroup[v.chatid] = msgGroup[v.chatid] || []
+            msgGroup[v.chatid].push(v)
+        })
+        
+        console.log(msgGroup)
+        //按照聊天用户分组，根据chatid
         return(
-            <h2>消息列表</h2>
+            <div>
+                <h2>消息列表</h2>
+            </div>
         )
     }
 }
